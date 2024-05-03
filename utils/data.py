fb5f2e12da5d79ff805c2874f4f81c210ac56ed7
poornagollakoti <chandra.gollakoti@gmail.com>
2024-05-03 07:30:05 +0530

    first commit

---
+++ utils/data.py
@@ -0,0 +1,506 @@
+ipl_auction_data =  [
+    {
+      "player": "Daryl Mitchell",
+      "nationality": "Overseas",
+      "type": "All-Rounder",
+      "team": "Chennai Super Kings",
+      "price": 140000000
+    },
+    {
+      "player": "Sameer Rizvi",
+      "nationality": "Indian",
+      "type": "Batter",
+      "team": "Chennai Super Kings",
+      "price": 84000000
+    },
+    {
+      "player": "Shardul Thakur",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Chennai Super Kings",
+      "price": 40000000
+    },
+    {
+      "player": "Mustafizur Rahman",
+      "nationality": "Overseas",
+      "type": "Bowler",
+      "team": "Chennai Super Kings",
+      "price": 20000000
+    },
+    {
+      "player": "Rachin Ravindra",
+      "nationality": "Overseas",
+      "type": "All-Rounder",
+      "team": "Chennai Super Kings",
+      "price": 18000000
+    },
+    {
+      "player": "Avanish Rao Aravelly",
+      "nationality": "Indian",
+      "type": "Wicket-Keeper",
+      "team": "Chennai Super Kings",
+      "price": 2000000
+    },
+    {
+      "player": "Kumar Kushagra",
+      "nationality": "Indian",
+      "type": "Wicket-Keeper",
+      "team": "Delhi Capitals",
+      "price": 72000000
+    },
+    {
+      "player": "Jhye Richardson",
+      "nationality": "Overseas",
+      "type": "Bowler",
+      "team": "Delhi Capitals",
+      "price": 50000000
+    },
+    {
+      "player": "Harry Brook",
+      "nationality": "Overseas",
+      "type": "Batter",
+      "team": "Delhi Capitals",
+      "price": 40000000
+    },
+    {
+      "player": "Sumit Kumar",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Delhi Capitals",
+      "price": 10000000
+    },
+    {
+      "player": "Shai Hope",
+      "nationality": "Overseas",
+      "type": "Wicket-Keeper",
+      "team": "Delhi Capitals",
+      "price": 7500000
+    },
+    {
+      "player": "Tristan Stubbs",
+      "nationality": "Overseas",
+      "type": "Wicket-Keeper",
+      "team": "Delhi Capitals",
+      "price": 5000000
+    },
+    {
+      "player": "Ricky Bhui",
+      "nationality": "Indian",
+      "type": "Wicket-Keeper",
+      "team": "Delhi Capitals",
+      "price": 2000000
+    },
+    {
+      "player": "Swastik Chhikara",
+      "nationality": "Indian",
+      "type": "Batter",
+      "team": "Delhi Capitals",
+      "price": 2000000
+    },
+    {
+      "player": "Rasikh Dar",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Delhi Capitals",
+      "price": 2000000
+    },
+    {
+      "player": "Spencer Johnson",
+      "nationality": "Overseas",
+      "type": "Bowler",
+      "team": "Gujarat Titans",
+      "price": 100000000
+    },
+    {
+      "player": "Shahrukh Khan",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Gujarat Titans",
+      "price": 74000000
+    },
+    {
+      "player": "Umesh Yadav",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Gujarat Titans",
+      "price": 58000000
+    },
+    {
+      "player": "Robin Minz",
+      "nationality": "Indian",
+      "type": "Wicket-Keeper",
+      "team": "Gujarat Titans",
+      "price": 36000000
+    },
+    {
+      "player": "Sushant Mishra",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Gujarat Titans",
+      "price": 22000000
+    },
+    {
+      "player": "Kartik Tyagi",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Gujarat Titans",
+      "price": 6000000
+    },
+    {
+      "player": "Azmatullah Omarzai",
+      "nationality": "Overseas",
+      "type": "All-Rounder",
+      "team": "Gujarat Titans",
+      "price": 5000000
+    },
+    {
+      "player": "Manav Suthar",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Gujarat Titans",
+      "price": 2000000
+    },
+    {
+      "player": "Mitchell Starc",
+      "nationality": "Overseas",
+      "type": "Bowler",
+      "team": "Kolkata Knight Riders",
+      "price": 247500000
+    },
+    {
+      "player": "Mujeeb Rahman",
+      "nationality": "Overseas",
+      "type": "Bowler",
+      "team": "Kolkata Knight Riders",
+      "price": 20000000
+    },
+    {
+      "player": "Sherfane Rutherford",
+      "nationality": "Overseas",
+      "type": "Batter",
+      "team": "Kolkata Knight Riders",
+      "price": 15000000
+    },
+    {
+      "player": "Gus Atkinson",
+      "nationality": "Overseas",
+      "type": "Bowler",
+      "team": "Kolkata Knight Riders",
+      "price": 10000000
+    },
+    {
+      "player": "Manish Pandey",
+      "nationality": "Indian",
+      "type": "Batter",
+      "team": "Kolkata Knight Riders",
+      "price": 5000000
+    },
+    {
+      "player": "K.S. Bharat",
+      "nationality": "Indian",
+      "type": "Wicket-Keeper",
+      "team": "Kolkata Knight Riders",
+      "price": 5000000
+    },
+    {
+      "player": "Chetan Sakariya",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Kolkata Knight Riders",
+      "price": 5000000
+    },
+    {
+      "player": "Angkrish Raghuvanshi",
+      "nationality": "Indian",
+      "type": "Batter",
+      "team": "Kolkata Knight Riders",
+      "price": 2000000
+    },
+    {
+      "player": "Ramandeep Singh",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Kolkata Knight Riders",
+      "price": 2000000
+    },
+    {
+      "player": "Sakib Hussain",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Kolkata Knight Riders",
+      "price": 2000000
+    },
+    {
+      "player": "Shivam Mavi",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Lucknow Super Giants",
+      "price": 64000000
+    },
+    {
+      "player": "M. Siddharth",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Lucknow Super Giants",
+      "price": 24000000
+    },
+    {
+      "player": "David Willey",
+      "nationality": "Overseas",
+      "type": "All-Rounder",
+      "team": "Lucknow Super Giants",
+      "price": 20000000
+    },
+    {
+      "player": "Ashton Turner",
+      "nationality": "Overseas",
+      "type": "Batter",
+      "team": "Lucknow Super Giants",
+      "price": 10000000
+    },
+    {
+      "player": "Arshin Kulkarni",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Lucknow Super Giants",
+      "price": 2000000
+    },
+    {
+      "player": "Mohd. Arshad Khan",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Lucknow Super Giants",
+      "price": 2000000
+    },
+    {
+      "player": "Gerald Coetzee",
+      "nationality": "Overseas",
+      "type": "All-Rounder",
+      "team": "Mumbai Indians",
+      "price": 50000000
+    },
+    {
+      "player": "Nuwan Thushara",
+      "nationality": "Overseas",
+      "type": "Bowler",
+      "team": "Mumbai Indians",
+      "price": 48000000
+    },
+    {
+      "player": "Dilshan Madushanka",
+      "nationality": "Overseas",
+      "type": "Bowler",
+      "team": "Mumbai Indians",
+      "price": 46000000
+    },
+    {
+      "player": "Mohammad Nabi",
+      "nationality": "Overseas",
+      "type": "All-Rounder",
+      "team": "Mumbai Indians",
+      "price": 15000000
+    },
+    {
+      "player": "Shreyas Gopal",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Mumbai Indians",
+      "price": 2000000
+    },
+    {
+      "player": "Shivalik Sharma",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Mumbai Indians",
+      "price": 2000000
+    },
+    {
+      "player": "Anshul Kamboj",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Mumbai Indians",
+      "price": 2000000
+    },
+    {
+      "player": "Naman Dhir",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Mumbai Indians",
+      "price": 2000000
+    },
+    {
+      "player": "Harshal Patel",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Punjab Kings",
+      "price": 117500000
+    },
+    {
+      "player": "Rilee Rossouw",
+      "nationality": "Overseas",
+      "type": "Batter",
+      "team": "Punjab Kings",
+      "price": 80000000
+    },
+    {
+      "player": "Chris Woakes",
+      "nationality": "Overseas",
+      "type": "All-Rounder",
+      "team": "Punjab Kings",
+      "price": 42000000
+    },
+    {
+      "player": "Tanay Thyagarajann",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Punjab Kings",
+      "price": 2000000
+    },
+    {
+      "player": "Vishwanath Pratap Singh",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Punjab Kings",
+      "price": 2000000
+    },
+    {
+      "player": "Ashutosh Sharma",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Punjab Kings",
+      "price": 2000000
+    },
+    {
+      "player": "Shashank Singh",
+      "nationality": "Indian",
+      "type": "Batter",
+      "team": "Punjab Kings",
+      "price": 2000000
+    },
+    {
+      "player": "Prince Choudhary",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Punjab Kings",
+      "price": 2000000
+    },
+    {
+      "player": "Rovman Powell",
+      "nationality": "Overseas",
+      "type": "Batter",
+      "team": "Rajasthan Royals",
+      "price": 74000000
+    },
+    {
+      "player": "Shubham Dubey",
+      "nationality": "Indian",
+      "type": "Batter",
+      "team": "Rajasthan Royals",
+      "price": 58000000
+    },
+    {
+      "player": "Nandre Burger",
+      "nationality": "Overseas",
+      "type": "Bowler",
+      "team": "Rajasthan Royals",
+      "price": 5000000
+    },
+    {
+      "player": "Tom Kohler-Cadmore",
+      "nationality": "Overseas",
+      "type": "Wicket-Keeper",
+      "team": "Rajasthan Royals",
+      "price": 4000000
+    },
+    {
+      "player": "Abid Mushtaq",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Rajasthan Royals",
+      "price": 2000000
+    },
+    {
+      "player": "Alzarri Joseph",
+      "nationality": "Overseas",
+      "type": "Bowler",
+      "team": "Royal Challengers Bengaluru",
+      "price": 115000000
+    },
+    {
+      "player": "Yash Dayal",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Royal Challengers Bengaluru",
+      "price": 50000000
+    },
+    {
+      "player": "Lockie Ferguson",
+      "nationality": "Overseas",
+      "type": "Bowler",
+      "team": "Royal Challengers Bengaluru",
+      "price": 20000000
+    },
+    {
+      "player": "Tom Curran",
+      "nationality": "Overseas",
+      "type": "All-Rounder",
+      "team": "Royal Challengers Bengaluru",
+      "price": 15000000
+    },
+    {
+      "player": "Saurav Chuahan",
+      "nationality": "Indian",
+      "type": "Batter",
+      "team": "Royal Challengers Bengaluru",
+      "price": 2000000
+    },
+    {
+      "player": "Swapnil Singh",
+      "nationality": "Indian",
+      "type": "All-Rounder",
+      "team": "Royal Challengers Bengaluru",
+      "price": 2000000
+    },
+    {
+      "player": "Pat Cummins",
+      "nationality": "Overseas",
+      "type": "All-Rounder",
+      "team": "Sunrisers Hyderabad",
+      "price": 205000000
+    },
+    {
+      "player": "Travis Head",
+      "nationality": "Overseas",
+      "type": "Batter",
+      "team": "Sunrisers Hyderabad",
+      "price": 68000000
+    },
+    {
+      "player": "Jaydev Unadkat",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Sunrisers Hyderabad",
+      "price": 16000000
+    },
+    {
+      "player": "Wanindu Hasaranga",
+      "nationality": "Overseas",
+      "type": "All-Rounder",
+      "team": "Sunrisers Hyderabad",
+      "price": 15000000
+    },
+    {
+      "player": "Jhathavedh Subramanyan",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Sunrisers Hyderabad",
+      "price": 2000000
+    },
+    {
+      "player": "Akash Singh",
+      "nationality": "Indian",
+      "type": "Bowler",
+      "team": "Sunrisers Hyderabad",
+      "price": 2000000
+    }
+  ]
